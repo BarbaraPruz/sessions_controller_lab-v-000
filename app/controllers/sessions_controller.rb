d24dec83d21bf501591ac02a71f35b7a62f45105
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-08-03 05:36:16 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,13 +9,9 @@ class SessionsController < ApplicationController
     #   session[:name] = params[:name]
     # end
     # redirect_to '/'
-    if !params[:name].present?
-      # render :new  -- error - test is looking for redirect and not render/200
-      redirect_to login_path
-    else
-      session[:name] = params[:name]
-      redirect_to '/'
-    end
+    redirect_to login_path if !params[:name].present?
+    session[:name] = params[:name]
+    redirect_to '/'
   end
 
   def destroy
